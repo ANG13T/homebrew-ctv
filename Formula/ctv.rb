2425936580c8560cf0274144fe7bc4205d39021b
Angelina Tsuboi <51937266+angelina-tsuboi@users.noreply.github.com>
2022-02-12 10:23:40 -0800

    Update ctv.rb

--- Formula/ctv.rb
+++ Formula/ctv.rb
@@ -3,7 +3,7 @@ class Ctv < Formula
   homepage "https://github.com/angelina-tsuboi/ctv"
   url "https://github.com/angelina-tsuboi/ctv/releases/download/v0.3.0/ctv-0.3.0-x86_64-apple-darwin.tar.gz"
   sha256 "ff57e023f447d63c76c49a1845fd471f46ea9fdf"
-  version "0.2.2"
+  version "0.3.0"
 
   def install
     bin.install "ctv"

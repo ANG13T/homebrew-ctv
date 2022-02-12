9faf2934bf6efa3f9023f7ed8cdd886eb4942235
Angelina Tsuboi <51937266+angelina-tsuboi@users.noreply.github.com>
2022-02-12 10:39:42 -0800

    Update ctv.rb

--- Formula/ctv.rb
+++ Formula/ctv.rb
@@ -1,9 +1,9 @@
 class Ctv < Formula
   desc "Highly configurable tree view file visualizer CLI tool made with Rust"
   homepage "https://github.com/angelina-tsuboi/ctv"
-  url "https://github.com/angelina-tsuboi/ctv/releases/download/v0.3.1/ctv-0.3.1-x86_64-apple-darwin.tar.gz"
-  sha256 "dedea0625a40d72267d6f8cb2077cb0a0db782c6"
-  version "0.3.0"
+  url "https://github.com/angelina-tsuboi/ctv/releases/download/v0.3.2/ctv-0.3.2-x86_64-apple-darwin.tar.gz"
+  sha256 "e71aff3a6f1be379e4e599d15774385f74bd0522"
+  version "0.3.2"
 
   def install
     bin.install "ctv"
